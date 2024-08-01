f6ea930f0ef22ee0906755ef102ced1fa6971769
RJRYT <robinjratr@gmail.com>
2024-08-01 20:05:52 +0530

    cors disabled

--- src/server.js
+++ src/server.js
@@ -13,21 +13,23 @@ const cleanupUnusedFiles = require("./app/helpers/CleanupFiles");
 const app = express();
 
 //Configure CORS
-const allowedOrigins = [process.env.FRONTEND_URL];
+// const allowedOrigins = [process.env.FRONTEND_URL];
 
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = `Request from origin ${origin} has been blocked by CORS policy`;
-      return callback(msg, false);
-    }
-    return callback(null, true);
-  },
-  credentials: true,
-};
+// const corsOptions = {
+//   origin: (origin, callback) => {
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       const msg = `Request from origin ${origin} has been blocked by CORS policy`;
+//       return callback(msg, false);
+//     }
+//     return callback(null, true);
+//   },
+//   credentials: true,
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+app.use(cors());
 
 //request logger
 app.use(morgan("dev"));
