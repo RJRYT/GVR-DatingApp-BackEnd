4fd539a9581bfbc8d29bda8271136ed4537268e4
Robin Jr <robinjratr@gmail.com>
2024-08-13 22:46:38 +0530

    enabled cors

--- src/server.js
+++ src/server.js
@@ -12,25 +12,20 @@ const cleanupUnusedFiles = require("./app/helpers/CleanupFiles");
 
 const app = express();
 
-//Configure CORS
-// const allowedOrigins = [process.env.FRONTEND_URL];
+Configure CORS
+const allowedOrigins = [process.env.FRONTEND_URL];
 
-// const corsOptions = {
-//   origin: (origin, callback) => {
-//     if (!origin) return callback(null, true);
-//     if (allowedOrigins.indexOf(origin) === -1) {
-//       const msg = `Request from origin ${origin} has been blocked by CORS policy`;
-//       return callback(msg, false);
-//     }
-//     return callback(null, true);
-//   },
-//   credentials: true,
-// };
-
-var corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 200
-}
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = `Request from origin ${origin} has been blocked by CORS policy`;
+      return callback(msg, false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
+};
 
 app.use(cors(corsOptions));
 
