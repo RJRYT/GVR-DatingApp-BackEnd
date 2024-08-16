10288e45244acff28f2d40ddd97c3936c59c8b8f
RJRYT <robinjratr@gmail.com>
2024-08-16 20:41:03 +0530

    bug fix

--- src/app/middlewares/auth.middleware.js
+++ src/app/middlewares/auth.middleware.js
@@ -1,3 +1,5 @@
+const jwt = require("jsonwebtoken");
+
 // Middleware to protect routes
 module.exports = (req, res, next) => {
   const token = req.header("x-auth-token") || req.cookies.accessToken;
@@ -9,6 +11,7 @@ module.exports = (req, res, next) => {
     req.user = decoded;
     next();
   } catch (err) {
+    console.log("[AuthMiddleware]: ",err);
     res.status(401).json({ status: 401, success: false, message: "Token is not valid" });
   }
 };
