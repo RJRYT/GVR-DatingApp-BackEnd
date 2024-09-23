cbddb8c1ea574facf0d4d419dc0740d31ed1187e
fathima <fathimanezrin2001@gmail.com>
2024-09-23 12:53:39 +0530

    admin profile editing integration

--- src/app/controllers/admin/admin.controllers.js
+++ src/app/controllers/admin/admin.controllers.js
@@ -1,5 +1,6 @@
 const { Admin } = require("../../models");
 const CatchAsync = require("../../util/catchAsync");
+const bcrypt = require ("bcryptjs");
 
 exports.test = (req, res) => {
   res.json({ status: 200, success: true, message: "hello world from admins" });
@@ -9,4 +10,39 @@ exports.fetchAdminDetails = CatchAsync(async(req, res) => {
   const adminId = req.admin.adminId;
   const admin = await Admin.findById(adminId).select("-password");
   res.json({ status: 200, success: true, message: "Admin details", user:admin });
+});
+
+exports.updateAdminProfile = CatchAsync( async (req, res) => {
+  const adminId = req.admin.adminId;  
+  const { firstName, lastName, email, phoneNumber, countryCode, nationality, designation, password } = req.body;
+
+  try {
+    
+    const admin = await Admin.findById(adminId);
+    if (!admin) {
+      return res.status(404).json({ success: false, message: "Admin not found" });
+    }
+
+    // Update the admin's profile fields
+    admin.firstName = firstName || admin.firstName;
+    admin.lastName = lastName || admin.lastName;
+    admin.email = email || admin.email;
+    admin.phoneNumber = phoneNumber || admin.phoneNumber;
+    admin.countryCode = countryCode || admin.countryCode;
+    admin.nationality = nationality || admin.nationality;
+    admin.designation = designation || admin.designation;
+    admin.password = password || admin.password;
+    // Save the updated admin profile
+    await admin.save();
+
+    // Return a success response
+    return res.status(200).json({ 
+      success: true, 
+      message: "Admin profile updated successfully.", 
+      user: admin 
+    });
+  } catch (error) {
+    console.error("Error while updating admin profile:", error);
+    return res.status(500).json({ success: false, message: "Failed to update admin profile", error: error.message });
+  }
 });
--- src/app/routes/admin/admin.routes.js
+++ src/app/routes/admin/admin.routes.js
@@ -5,5 +5,6 @@ const { AdminAuthMiddleware } = require("../../middlewares");
 const router = express.Router();
 
 router.get("/", AdminAuthMiddleware, AdminController.fetchAdminDetails);
+router.put("/edit/profile", AdminAuthMiddleware, AdminController.updateAdminProfile)
 
 module.exports = router;
